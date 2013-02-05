a528c14d3f06bbc83bf638fefb3749f6472b8c44
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-02-04 19:24:32 -0500

    More stuff

--- main.rb
+++ main.rb
@@ -5,20 +5,43 @@ require 'json'
 
 
 conn = PG.connect(:dbname =>'moviez', :host => 'localhost')
-conn.exec( "select * from movies" ) do |result|
-  result.each do |row|
-  end
-end
+# conn.exec( "select * from movies" ) do |result|
+#   result.each do |row|
+#   end
+# end
 
-print 'Movie Title: '
-title = gets.chomp.split.join("+")
-results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{title}")
-results = JSON(results.body)
+# puts "(S)earch Movies\n(V)iew Database\n(Q)uit"
+# response = gets.chomp.downcase
 
-puts results.values.join("\n")
+# while response != 'q'
 
-sql = "insert into movies (title, year, rated, released, runtime, genre, director, writer, actors, plot, poster, rating, votes, imdb_id) values ('#{results['Title']}', '#{results['Year']}', '#{results['Rated']}', '#{results['Released']}', '#{results['Runtime']}', '#{results['Genre']}', '#{results['Director']}', '#{results['Writer']}', '#{results['Actors']}', '#{results['Plot']}', '#{results['Poster']}', '#{results['imdbRating']}', '#{results['imdbVotes']}', '#{results['imdbID']}')"
+#   if response == 's'
 
-conn.exec(sql)
+    print 'Movie Title: '
+    title = gets.chomp.split.join("+")
+    results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{title}")
+    results = JSON(results.body)
+
+    puts results.values.join("\n")
+
+    sql = "insert into movies (title, year, rated, released, runtime, genre, director, writer, actors, plot, poster, rating, votes, imdb_id) values ('#{results['Title']}', '#{results['Year']}', '#{results['Rated']}', '#{results['Released']}', '#{results['Runtime']}', '#{results['Genre']}', '#{results['Director']}', '#{results['Writer']}', '#{results['Actors']}', '#{results['Plot']}', '#{results['Poster']}', '#{results['imdbRating']}', '#{results['imdbVotes']}', '#{results['imdbID']}')"
+
+    conn.exec(sql)
+
+#   elsif response == 'v'
+
+#     conn.exec( "select * from movies" ) do |result|
+#       result.each do |row|
+#         puts row.values
+#       end
+#     end
+
+#   else
+
+#     puts "Thank you"
+
+#   end
+
+# end
 
 conn.close
