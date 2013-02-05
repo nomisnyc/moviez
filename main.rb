2bd83c8066d51d8f2633a7a044f341af16441746
Simon Wong <nomisnyc85@gmail.com>
2013-02-04 20:22:46 -0500

    did some color

--- main.rb
+++ main.rb
@@ -2,47 +2,49 @@ require 'pg'
 require 'pry'
 require 'httparty'
 require 'json'
+require 'rainbow'
 
 
-conn = PG.connect(:dbname =>'moviez', :host => 'localhost')
-# conn.exec( "select * from movies" ) do |result|
-#   result.each do |row|
-#   end
-# end
-
-# puts "(S)earch Movies\n(V)iew Database\n(Q)uit"
-# response = gets.chomp.downcase
-
-# while response != 'q'
-
-#   if response == 's'
-
-    print 'Movie Title: '
-    title = gets.chomp.split.join("+")
-    results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{title}")
-    results = JSON(results.body)
-
-    puts results.values.join("\n")
-
-    sql = "insert into movies (title, year, rated, released, runtime, genre, director, writer, actors, plot, poster, rating, votes, imdb_id) values ('#{results['Title']}', '#{results['Year']}', '#{results['Rated']}', '#{results['Released']}', '#{results['Runtime']}', '#{results['Genre']}', '#{results['Director']}', '#{results['Writer']}', '#{results['Actors']}', '#{results['Plot']}', '#{results['Poster']}', '#{results['imdbRating']}', '#{results['imdbVotes']}', '#{results['imdbID']}')"
-
-    conn.exec(sql)
-
-#   elsif response == 'v'
-
-#     conn.exec( "select * from movies" ) do |result|
-#       result.each do |row|
-#         puts row.values
+# conn = PG.connect(:dbname =>'moviez', :host => 'localhost')
+#  conn.exec( "select * from movies" ) do |result|
+#    result.each do |row|
 #       end
 #     end
 
-#   else
+def menu()
+  puts `clear`
+  puts "\n\n ---Welcome to Moviez DB---\n\n"
+   print "(S)earch Movies\n(V)iew Database\n(Q)uit\n\n".foreground(:blue)
+  gets.chomp.downcase
+end
 
-#     puts "Thank you"
 
-#   end
 
-# end
+response = menu
 
-conn.close
+while response != 'q'
+  case response
+  when 's'
 
+    print 'Movie Title: '.foreground(:blue)
+    title = gets.chomp.split.join("+")
+    results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{title}")
+    results = JSON(results.body)
+    puts results.values.join("\n").foreground(:blue)
+    sql = "insert into movies (title, year, rated, released, runtime, genre, director, writer, actors, plot, poster, rating, votes, imdb_id) values ('#{results['Title']}', '#{results['Year']}', '#{results['Rated']}', '#{results['Released']}', '#{results['Runtime']}', '#{results['Genre']}', '#{results['Director']}', '#{results['Writer']}', '#{results['Actors']}', '#{results['Plot']}', '#{results['Poster']}', '#{results['imdbRating']}', '#{results['imdbVotes']}', '#{results['imdbID']}')"
+    conn.exec(sql)
+  when 'v'
+
+    conn.exec( "select * from movies" ) do |result|
+      result.each do |row|
+        puts row.values
+        get
+      end
+      response = menu
+      conn.close
+
+
+
+end
+end
+end
